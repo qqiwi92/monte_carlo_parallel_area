824102af4aca6b2eaf3eada7ae18a42503893217
Dmitry Levkin <dmtrlevkn@gmail.com>
2026-09-19 20:19:11 +0300

    switched to C styled system calls

--- main.cpp
+++ main.cpp
@@ -11,6 +11,16 @@ struct p_t {
 p_t center = {1, 1};
 static double radius = 1;
 
+struct calc_args {
+    size_t tests;
+    size_t seed;
+};
+
+struct calc_result {
+    size_t tests;
+};
+
+
 bool isInside(p_t point, p_t center, double r) {
     double xdiff = point.x - center.x;
     double ydiff = point.y - center.y;
@@ -21,7 +31,7 @@ bool isInside(p_t point, p_t center, double r) {
 
 size_t calc(size_t tests, size_t seed) {
     std::mt19937 gen(seed);
-    std::uniform_real_distribution<double> dis(0.0, 1.0);
+    std::uniform_real_distribution<double> dis(0.0, 2.0);
 
     size_t total = 0;
 
@@ -34,20 +44,40 @@ size_t calc(size_t tests, size_t seed) {
     return total;
 }
 
+void *calc_wrappper(void *data) {
+    calc_args *args = static_cast<calc_args *>(data);
+    calc_result* result = new calc_result{calc(args->tests, args->seed)};
+    return result;
+}
+
+
 double area(size_t threads, size_t tests) {
-    size_t per_thread = tests  / threads;
+    size_t perThread = tests / threads;
 
-    std::vector<std::future<size_t>> f;
-    f.reserve(threads);
-
+    std::vector<pthread_t> threadsVec;
+    std::vector<calc_args*> threadArguments;
+    threadsVec.reserve(threads);
+    threadArguments.reserve(threads);
     size_t hit = 0;
-    for (size_t i = 0; i < tests; i++) {
-        f.push_back(std::async(std::launch::async, calc, per_thread, i));
+
+    for (size_t i = 0; i < threads; i++) {
+        pthread_t th;
+        auto * args = new calc_args{perThread, i};
+        int err = pthread_create(&th, nullptr, calc_wrappper, args);
+        threadsVec.push_back(th);
+        threadArguments.push_back(args);
     }
-    for (std::future<size_t>& item : f) {
-        hit += item.get();
+    for (size_t i = 0; i < threads; i++) {
+        pthread_t th =  threadsVec[i];
+        void* result;
+        int err =pthread_join(th, &result);
+        auto * res = static_cast<calc_result*>(result);
+        hit += res->tests;
+
+        delete res;
+        delete threadArguments[i];
     }
-    return static_cast<double>(hit) / static_cast<double>(tests);
+    return static_cast<double>(hit) / static_cast<double>(tests) * 4.0;
 }
 
 
